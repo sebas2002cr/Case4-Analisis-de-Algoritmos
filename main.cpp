7a674c99481aa5b5a86b18ab0452bf53b4f37a24
CrisEsqui <crisesqui908@gmail.com>
2021-03-19 15:38:58 -0600

    Ultimos ajustes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+/*
+ * Caso 4 Analisis de algoritmos
+ * Estudiantes: 
+ *		-Cristhian Esquivel
+ *		-Sebastián Obando
+ */
+
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
 #include <sstream>
+#include <ctime>
 
 using namespace std;
 
@@ -10,154 +18,117 @@ void crearImagen(int, int);
 int main(int argc, char** argv) {
 
 
-srand((unsigned int)time(NULL));
+	srand((unsigned int)time(NULL));
     int alto, ancho;
-
-    //cout << "Digite el alto: "; cin >> alto;
-    //cout << endl;
-    //cout << "Digite el ancho: "; cin >> ancho;
-    ancho = 650;
-    alto = 700;
+    
+    //Creación del patrón en dimensiones de 750x900
+    ancho = 900;
+    alto = 750;
     cout << endl;
 
     crearImagen(alto, ancho);
+    
+    //Creación del patrón en dimensiones de 2000x1200
+    ancho = 1200;
+    alto = 2000;
+    cout << endl;
+
+    crearImagen(alto, ancho);
+    
+    //Puede descomentar estas lineas si quiere que introducir las dimensiones por medio de la consola
+    //cout << "Digite el alto: "; cin >> alto;
+    //cout << endl;
+    //cout << "Digite el ancho: "; cin >> ancho;
+    //crearImagen(alto, ancho);
 
     return 0;
 }
 
+
+/*
+ * Analisis del algoritmo.
+ *
+ * El algoritmo calcula el 10% de los pixeles a crear y genera puntos de manera "aleatoria" para simular el patrón asignado.
+ * El color gris representa un 89% de los pixeles coloreados y representa el fondo de la imagen. Los puntos negros tienen un
+ * rango de generación menor y buscan juntarse entre sí para poder mostrar un color negro más intenso.
+ *
+ * En los comentarios de cada línea se muestran la cantidad de tiempos que requieren
+ * F(n) = 48n + 40
+ * 
+ * n = pAlto * pAncho * 0.1 (o sea, el 10% de la cantidad de píxeles que va a tener el dibujo)
+ *
+ * O grande = O(n)
+ */
 void crearImagen(int pAlto, int pAncho)
 {
     int randX, randY, cantidadPuntos;
     int random;
-    string color = "";
-    cantidadPuntos = (pAlto*pAncho) * 0.1;
+    string color = ""; //-->1
+    cantidadPuntos = (pAlto*pAncho) * 0.1; //-->3
 
 
     int last_black[2];
-    last_black[0] = 2 * (pAncho/3);
-    last_black[1] = 2 * (pAlto/3);
+    last_black[0] = 2 * (pAncho/3); //-->4
+    last_black[1] = 2 * (pAlto/3); // --4
     ostringstream cadena;
-    cadena << "[\n";
-    //Creaciï¿½n de puntos======================================================================================
+    cadena << "[\n"; //-->1
+    //Creacion de puntos======================================================================================
 
     //Datos de porciones
-    int p50 = cantidadPuntos *0.2;
-    int p50x =(pAncho*0.1);
+    int p50 = cantidadPuntos *0.2; //-->2
+    int p50x =(pAncho*0.1); //-->3
 
 
-    int p20 = cantidadPuntos*0.2;
-    int p20x =(pAlto*0.2);
-    int p20y =(pAlto*0.2);
-    //cout<< "p50: "<<p50<<endl;
-    while (cantidadPuntos > 0)
+    int p20 = cantidadPuntos*0.2; //-->2
+    int p20x =(pAlto*0.2); //-->2
+    int p20y = (pAlto*0.2); //-->2
+    
+    
+    while (cantidadPuntos > 0)//-->(1 + 47)(n) + 40 => 48(n) + 40
     {
-        randX = rand()%pAncho;
-        randY = rand()%pAlto;
-        random = rand()%100;
+        randX = rand()%pAncho; //-->3
+        randY = rand()%pAlto; //-->3
+        random = rand()%100; //-->3
 
-        if (random<89.5)
+        if (random<89.5) //-->1
         {
             int x = rand() % 2;     //-- generate random integer between 0 and 1
             if(x == 0) color = "black";
-            else color = "Grey";
-
-        }
-        else
-                {
-                    do{
-                       // randX = p50x + rand()% ((pAncho) -p50x);
-                       // randY = pAlto/2 + rand()% ((pAlto) - p20y )  ;
-                        randX =  rand()% ((pAncho) -1);
-                        randY =  rand()% ((pAlto) -1)  ;
-
-                    }while (randX < 0 or randX > pAncho or randY < 0 or randY > pAlto );
-
-
-                    color = "black";
-
-                    last_black[0] = randX;
-                    last_black[1] = randY;
-
-
-                    do{
-                         randX = p50x + rand()% ((pAncho/2) -p50x);
-                         randY = pAlto/2 + rand()% ((pAlto) - p20y )  ;
-                        //randX =  rand()% ((pAncho) -1);
-                        //randY =  rand()% ((pAlto) -1)  ;
-
-                    }while (randX < 0 or randX > pAncho or randY < 0 or randY > pAlto );
-
-
-                    color = "black";
-
-                    last_black[0] = randX;
-                    last_black[1] = randY;
-
-
-                }
-
-
-
-
-
-        cadena << "\t\t[" << "\"" << color << "\"," << randX << "," << randY << "]";
-        cantidadPuntos--;
-        cadena << (cantidadPuntos<=0 ? "":",") << endl;
-    }
-
-    cadena << "\t]" << endl;
-   /* while (cantidadPuntos > 0)
-    {
-        randX = p50x + rand()% ((pAncho/2+1) -p50x);
-        randY = p20x + rand()% ((pAlto/2+1) - p20x )  ;
-        random = rand()%100;
-
-        if (random<70)
-        {
-            color = "gray";
+            else color = "grey";
         }
         else
         {
             do{
-                randX =  last_black[0] + (-10 + rand()%20);
-                randY =  last_black[1] + (-10 + rand()%20);
+                randX = p50x + rand()% ((pAncho/2) -p50x);//-->6
+                randY = pAlto/2 + rand()% ((pAlto) - p20y );//-->5
+
+            }while (randX < 0 or randX > pAncho or randY < 0 or randY > pAlto );//-->7
+            
+            color = "black"; //->1
+            last_black[0] = randX; //-->2
+            last_black[1] = randY; //-->2
             }
-            while (randX < 0 or randX > pAncho or randY < 0 or randY > pAlto);
 
-
-            color = "black";
-
-            last_black[0] = randX;
-            last_black[1] = randY;
-        }
-        cadena << "\t\t[" << "\"" << color << "\"," << randX << "," << randY << "]";
-        cantidadPuntos--;
-        cadena << (p50<=0 ? "":",") << endl;
+        cadena << "\t\t[" << "\"" << color << "\"," << randX << "," << randY << "]";//-->8
+        cantidadPuntos--;//-->2
+        cadena << (cantidadPuntos<=0 ? "":",") << endl;//-->4
     }
 
-    cout<< "cantidadPuntos: "<< cantidadPuntos <<endl;
-    cout<< "p60: "<< p50 <<endl;
-    cout<< "p20: "<< p20 <<endl;
-    cout<< "p50x: "<< p50x <<endl;
-    cout<< "p20x: "<< p20x <<endl;
-    cout<< "RandomX: "<< randX <<endl;
-    cout<< "RandomY: "<< randY <<endl;
-    cadena << "\t]" << endl;
-*/
+    cadena << "\t]" << endl; //-->2
+    
     //======================================================================================
     //Generar JSON
     ofstream file;
-    //"/Users/sebastianobandopaniagua/Desktop/Analisis de Algoritmos/Caso4/Case4-Analisis-de-Algoritmos/JS/resultado.json"
-    file.open("JS/resultado.json");
-    file << "{\n";
-    file << "\t\"alto\":" << pAlto << ",\n";
-    file << "\t\"ancho\":" << pAncho << ",\n";
-    file << "\t\"puntos\":" << cadena.str();
+    file.open("JS/resultado.json");//-->1
+    file << "{\n";//-->1
+    file << "\t\"alto\":" << pAlto << ",\n";//-->3
+    file << "\t\"ancho\":" << pAncho << ",\n";//-->3
+    file << "\t\"puntos\":" << cadena.str();//-->3
 
-    file << "}";
-    file.close();
+    file << "}"; //-->1
+    file.close();//-->1
 
-    cout << "Archivo JSON listo." << endl;
+    cout << "Archivo JSON listo." << endl;//-->1
 }
 
 
